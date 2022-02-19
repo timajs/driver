ea55c72fa983f5ea3852d7f8281e50bbed841950
Aliya <ishenovaaliya6gmail.com>
2022-02-19 13:34:14 +0600

    info for auth

--- src/pages/auth/auth.jsx
+++ src/pages/auth/auth.jsx
@@ -3,8 +3,9 @@ import css from './auth.module.css';
 
 function Auth (){
     return(
-        <div className={css.auth}>
-            <p className={css.title}>Авторизация</p>
+      <div className={css.auth}>
+        <p className={css.title}>Авторизация</p>
+        <div className={css.container}>
             <div className={css.command__auth}>
                 <input className={css.inp} type="text" placeholder="Логин"/>
                 <br />
@@ -14,7 +15,24 @@ function Auth (){
                 <br />
                 <button className={css.btn}>Авторизoваться</button>
             </div>
+            <div className={css.information}>
+                <p>Если вы хотите авторизоваться, нужно связаться по следующим контактам:</p>
+                <div className={css.call}>
+                    <img className={css.link__img} src="/img/whatsapp.svg" alt="" />
+                    <a className={css.link} href="https://www.whatsapp.com/?lang=ru/" target="_blank">+996500706958</a>
+                </div>
+                <div className={css.call}>
+                    <img className={css.link__img} src="/img/gmail.png" alt="" />
+                    <a className={css.link} href="https://www.google.com/intl/ru/gmail/about/" target="_blank">autoschoolKoleso@gmail.com</a>
+                </div>
+                <div className={css.call}>
+                    <img className={css.link__img} src="/img/telegram.png" alt="" />
+                    <a className={css.link} href="https://apps.apple.com/ru/app/telegram/id686449807" target="_blank">+996500706958</a>
+                </div>
+                <br />
+            </div>
         </div>
+      </div>
     )
 }
 
